186c0b3b1cf34e4b688fe74bcfc594db7a3767b9
Elif <elffemre@gmail.com>
2022-07-23 19:12:29 +0300

    class ekleme
    - tableda eklediğimiz dataları classlar ile ekledik
    -IEntity ile birleştirdik

---
+++ Entities/Concrate/BaBsReconciliationDetails.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrate
+{
+    public class BaBsReconciliationDetails : IEntity
+    {
+        public int Id { get; set; }
+        public int BabsReconciliationId { get; set; }
+        public DateTime Date { get; set; }
+        public string Decription { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
